971b8f3026823d151ad8fa8cedb237279d9e8fa1
Jakub Matuszczak <kubamatuszczak2@gmail.com>
2024-07-18 17:37:50 +0200

    Update analogue.py

--- analogue.py
+++ analogue.py
@@ -53,3 +53,49 @@ def trenujModel(akcja, x, y):
 
     # Zwracanie modelu i wartości mae, mse, r2
     return model, mae, mse, r2
+
+
+# Funkcja do zaokrąglania liczb
+def truncate(n, decimals=0):
+    multiplier = 10**decimals
+    return int(n * multiplier) / multiplier
+
+if __name__ == "__main__":
+    # Ustawianie wejścia
+    wejście = "AAPL"
+    # Zwracanie df z wykrywacza paternów
+    x = get_data(wejście, '2015-01-01', '2023-01-12', interval="1d")
+    x.columns = map(str.lower, x.columns)
+    df = x.copy()
+    df['garman_klass_vol'] = ((np.log(df['high'])-np.log(df['low']))**2)/2-(2*np.log(2)-1)*((np.log(df['adj close'])-np.log(df['open']))**2)
+    x = df.copy()
+
+
+
+
+
+    # przesunięcie danych o 1 w górę i dodanie do y (wartości, które chcemy przewidzieć), chodzi o to żeby przewidywało następne zamknięcie
+    y = x['close'].shift(-1)
+    # usunięcie high, low i close z x (x to dane wejściowe do modelu)
+    x = x.drop(['high', 'low', 'close'], axis=1)
+    # edit timestamp to datetime to timestamp
+    x['date'] = pd.to_datetime(x['date'])
+    x['date'] = x['date'].map(dt.datetime.toordinal)
+    # naprawienie indexów, żeby się zgadzały
+    x = x.reset_index(drop=True)
+    y = y.reset_index(drop=True)
+    # trenowanie modelu i zwrócenie modelu, mae, mse, r2 do zmiennych
+    model, mae, mse, r2 = trenujModel(wejście, x, y)
+    # wypisanie informacji o modelu
+    print("Model został \033[92m pomyślnie \033[0mtrenowany i zapisany do pliku.")
+    print(f"Średnia pomyłka modelu regresyjnego: \033[93m{truncate(mae, 2)}$ \033[0m")
+    print(f"Dokładność procentowa modelu regresyjnego: \033[96m{truncate(100 - (mae/165)*100, 2)}% \033[0m")
+    print(f"Dopasowanie modelu regresyjnego: \033[92m{truncate(r2, 4)*100}% \033[0m")
+    # wypisanie procentu pomyłki dla różnicy dwóch ostatnich wartości zamknięcia
+  
+    #obliczanie procentu pomyłki
+    różnica = 2.3
+    procent_pomyłki = mae/różnica*100
+    # wypisanie informacji o procentach pomyłki
+    print(f"Procent pomyłki: \033[93m{procent_pomyłki}% \033[0m")
+    print(f"Różnica: \033[93m{różnica} \033[0m")
